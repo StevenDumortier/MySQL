779732f9e99d2baa21ae1903ec05573e941f0ffa
Steven Dumortier <stevendumortier59@gmail.com>
2022-06-02 10:53:13 +0200

    Créer une base de donnée

---
+++ clients.sql
@@ -0,0 +1,14 @@
+-- Authentification/Connexion
+>mysql -u root -- si l'utilisateur n'a pas de mot de passe
+>mysql -u root -p -- si l'utilisateur possède un mdp
+
+-- 1. Lister toutes les bases de donnés
+SHOW DATABASE;
+
+-- 2. Création de la base de données
+
+CREATE DATABASE ventes;
+
+
+
+-- 3 . Création de la table clients
