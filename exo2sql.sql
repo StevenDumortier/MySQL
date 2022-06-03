0d7b73cdcb057295ee7dc6bea71c8464a3caa622
Steven Dumortier <stevendumortier59@gmail.com>
2022-06-03 23:31:53 +0200

    exercice terminé

--- exo2sql.sql
+++ exo2sql.sql
@@ -18,7 +18,7 @@ id integer AUTO_INCREMENT not null PRIMARY key,
 typePresta VARCHAR(30) not null,
    designation varchar(30) not null,
 clientid integer,
-FOREIGN KEY (clientid)REFERENCES clients(id) on delete set null
+FOREIGN KEY (clientid)REFERENCES clients(id) on delete set null,
 nbDays integer(3),
 unitPrice integer(10),
 state varchar(1) not null
@@ -26,23 +26,53 @@ state varchar(1) not null
 
 
 INSERT into clients (companyName, firstName, lastName, email , phone, adress,zipCode , city, country, state) 
-values ("Capgemini", "Fabrice", "Martin" , "martin@mail.com", "06 56 85 84 33", "abc" , "xyz" , "Nantes" , "France",  "0"),("M2I Formation", "Julien" , "Lamard", "lamard@mail.com", "06 11 22 33 44", "abc" , "xyz" , "Paris" , "France",  "1"),
-("ATOS", "Homer" , "Simpsons", "simpsons@mail.com", "06 51 52 33 44", "abc" , "xyz" , "Springfield" , "USA",  "1")
+values ("Capgemini", "Fabrice", "Martin" , "martin@mail.com", "06 56 85 84 33", "abc" , "xyz" , "Nantes" , "France",  "0"),
+("M2I Formation", "Julien" , "Lamard", "lamard@mail.com", "06 11 22 33 44", "abc" , "xyz" , "Paris" , "France",  "1"),
+("ATOS", "Homer" , "Simpsons", "simpsons@mail.com", "06 51 52 33 44", "abc" , "xyz" , "Springfield" , "USA",  "1"),
 ("Sopra Steria", "jean" , "bon", "bon@mail.com", "06 11 23 53 99", "abc" , "xyz" , "Lille" , "France",  "1");
 
-INSERT into orders(typePresta,designation,clientid,nbDays,unitPrice,state) values ("Formation","Angular init", 2 ,3 ,1200, "0"),("Formation", "React avancé", 2, 3, 1000, "2"),("Coaching", "React Techlead",1 ,20 ,900,"2"),("Coaching", "Nest.js Techlead", 1 ,50 ,800,"1"),("Coaching", "React Teachead" , 3, null, null, null),("Coaching", "Angular Techlead" ,4 , null, null ,null),("Coaching", "Jakarta EE" ,3 , null, null ,null),("Coaching", "Angular" , 3 , 15, 200 ,0),("Coaching", "bAngular" , 3 , 15, 200 ,0),("Coaching", "gular" , 3 , 15, 200 ,0),("Coaching","lar", 3 , 15, 200 ,0);
+INSERT into orders(typePresta,designation,clientid,nbDays,unitPrice,state) 
+values ("Formation","Angular init", 2 ,3 ,1200, "0"),
+("Formation", "React avancé", 2, 3, 1000, "2"),
+("Coaching", "React Techlead",1 ,20 ,900,"2"),
+("Coaching", "Nest.js Techlead", 1 ,50 ,800,"1"),
+("Coaching", "React Teachead" , 3, null, null, null),
+("Coaching", "Angular Techlead" ,4 , null, null ,null),
+("Coaching", "Jakarta EE" ,3 , null, null ,null),
+("Coaching", "Angular" , 3 , 15, 200 ,0),
+("Coaching", "bAngular" , 3 , 15, 200 ,0),
+("Coaching", "gular" , 3 , 15, 200 ,0),
+("Coaching","lar", 3 , 15, 200 ,0);
 
-Create VIEW v_total as SELECT (orders.nbDays*orders.unitPrice) as "total taxe excluded",(orders.nbDays*orders.unitPrice + orders.nbDays*orders.unitPrice*0.2) as totalwithTaxe FROM orders;
 
-SELECT CONCAT(clients.prenom," ",clients.nom) AS nom, clients.email,telephones.numero FROM clients JOIN telephones ON clients.id=telephones.id_client WHERE clientid = 2;
+-- => Afficher toutes les formations sollicités par le client M2i formation
+SELECT clients.companyName,  concat(clients.firstName, " ",clients.lastname)as nom,orders.designation as Formation FROM `orders` JOIN clients on clients.id=orders.clientid WHERE clientid=2;
 
-SELECT * FROM `orders` WHERE clientid=2
+-- Afficher les noms et contacts de tous les contacts des clients qui ont sollicité un coaching (affiché les clients /coaching)
 
-SELECT distinct (concat (clients.firstName," ",clients.lastName)) as nom where typePresta = Coaching;
+SELECT DISTINCT CONCAT(clients.firstName," ",clients.lastName) AS nom, orders.typePresta FROM clients JOIN orders ON clients.id=orders.clientid WHERE typePresta="Coaching";
 
-SELECT DISTINCT CONCAT(clients.firstName," ",clients.lastName) AS nom FROM clients JOIN orders ON clients.id=orders.clientid WHERE typePresta="Coaching";
 
-SELECT DISTINCT CONCAT(clients.firstName," ",clients.lastName) AS nom FROM clients JOIN orders ON clients.id=orders.clientid WHERE typePresta="Coaching" and designation = "Nest.js Techlead";
--- SELECT DISTINCT CONCAT(clients.firstName," ",clients.lastName) AS nom, designation, typePresta FROM clients JOIN orders ON clients.id=orders.clientid WHERE typePresta="Coaching" and designation = "Nest.js Techlead";
+-- Afficher les noms et contacts de tous les contacts des clients qui ont sollicité un coaching pour les accompagnements React.js
+SELECT DISTINCT CONCAT(clients.firstName," ",clients.lastName) AS nom, orders.typePresta, orders.designation FROM clients JOIN orders ON clients.id=orders.clientid WHERE typePresta="Coaching" and designation = "Nest.js Techlead";
 
+
+-- correct
+
+Create VIEW v_total5 as SELECT (orders.nbDays*orders.unitPrice) as "totaltaxeexcluded",(orders.nbDays*orders.unitPrice + orders.nbDays*orders.unitPrice*0.2) as totalwithTaxe, orders.id;
+
+SELECT orders.designation, v_total5.totaltaxeexcluded, v_total5.totalwithTaxe FROM v_total5 JOIN orders ON v_total5.id=orders.id WHERE orders.typePresta = "formation";
+
+SELECT orders.designation, v_total5.totaltaxeexcluded, v_total5.totalwithTaxe, state FROM v_total5 JOIN orders ON v_total5.id=orders.id WHERE orders.state = "2" and v_total5.totaltaxeexcluded >=30000;
+
+
+
+
+
+--Create VIEW v_total2 as SELECT (orders.nbDays*orders.unitPrice) as "total taxe excluded",(orders.nbDays*orders.unitPrice + orders.nbDays*orders.unitPrice*0.2) as totalwithTaxe, concat(typePresta," ",designation) FROM orders;
+--Create VIEW v_total3 as SELECT (orders.nbDays*orders.unitPrice) as "total taxe excluded",(orders.nbDays*orders.unitPrice + orders.nbDays*orders.unitPrice*0.2) as totalwithTaxe,typePresta,designation FROM orders;
+
+--SELECT * FROM `v_total3` WHERE `typePresta`= "formation";
+
+--Create VIEW v_total4 as SELECT (orders.nbDays*orders.unitPrice) as "total taxe excluded",(orders.nbDays*orders.unitPrice + orders.nbDays*orders.unitPrice*0.2) as totalwithTaxe,typePresta,designation FROM orders;
 
